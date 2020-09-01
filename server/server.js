270279b19f737fa2240d71ee895c7c24b1ad674c
Derek Williamson <derekwilliamson22@gmail.com>
2020-09-01 14:24:53 -0500

    first server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
-console.log('in server.js');
+// import the express library
+// from node_modules/express
+const express = require(`express`);
+
+// go go gadget express!
+// makes an app, aka a "server"
+const app = express();
+
+// start up the server
+app.listen(3000, function() {
+    console.log(`I'm listening`);
+});
 
 
-// npm === "node package manager"
 
