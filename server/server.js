4e254ba77e94d1bc0814e04fe6fe23c5522b25fb
Derek Williamson <derekwilliamson22@gmail.com>
2020-09-01 14:52:35 -0500

    my first server set up

--- server/server.js
+++ server/server.js
@@ -6,9 +6,18 @@ const express = require(`express`);
 // makes an app, aka a "server"
 const app = express();
 
+// incantation for serving static files
+app.use(express.static(`server/public`));
+
+// when I get a request at "/hello"
+// send back a response of "Hello World!"
+app.get(`/hello`, function(req, res){
+    res.send(`<h1>Hello World!</h1>`);
+});
+
 // start up the server
 app.listen(3000, function() {
-    console.log(`I'm listening`);
+    console.log(`I'm listening on 3000`);
 });
 
 
