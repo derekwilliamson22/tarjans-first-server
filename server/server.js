5eddfda96cbc819abfe4505f2cce4907222f1c06
Derek Williamson <derekwilliamson22@gmail.com>
2020-09-01 20:41:34 -0500

    server work

--- server/server.js
+++ server/server.js
@@ -9,10 +9,49 @@ const app = express();
 // incantation for serving static files
 app.use(express.static(`server/public`));
 
+// An endpoint at /hello
 // when I get a request at "/hello"
 // send back a response of "Hello World!"
 app.get(`/hello`, function(req, res){
     res.send(`<h1>Hello World!</h1>`);
+});
+
+
+// a persistence layer
+let quotes = [
+    { 
+      quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', 
+      author: 'Emma Watson' 
+    },
+    { 
+      quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', 
+      author: 'Scott Adams' 
+    },
+    { 
+      quote: 
+      'Intelligence plus character-that is the goal of true education.', 
+      author: 'Martin Luther King, Jr.' 
+    }
+  ];
+let i = 0;
+
+function getNextQuote(){
+let nextQuote = quotes[i];
+// increment index
+// get a new quote
+i++;
+
+// got back to beginning
+if (i >= quotes.length){
+    i = 0;
+}
+return nextQuote;
+}
+
+
+app.get(`/quotes`, function(req, res){
+    let nextQuote = getNextQuote();
+    res.send(nextQuote);
 });
 
 // start up the server
